46d146e7d6cc49bca5d9a62ec37bae9fb2c3db29
Susan Nio <susan.wen88@gmail.com>
2018-07-10 11:50:46 +0700

    Adding palindromeSwapper.js

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -7,18 +7,21 @@
  */
 function palindromeSwapper(str) {
   let newStr = '';
+  if (isPalindrome(str)) return true;
+
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    newStr="";
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
         j++;
-      } else {
-        newStr += str[j];
+      }else{
+        newStr+=str[j]; 
       }
-    }
+    } 
     if (isPalindrome(newStr)) return true;
-  }
+  } 
   return false;
 }
 
@@ -29,8 +32,9 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join("") === str) 
+  return true;
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
