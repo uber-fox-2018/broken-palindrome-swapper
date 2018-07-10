e4c41310232591e540f8b6ca77f6d0e9a337d98f
riadji.github.io <riadji.ramadhan@gmail.com>
2018-07-10 17:26:39 +0700

    Add Broken Palindrome

---
+++ brokenpalindrome.js
@@ -0,0 +1,41 @@
+/**
+ * @function palindromeSwapper
+ * menukar 2 huruf berurutan dari n = 0 sampai n-1
+ * contoh: makan => amkan, mkaan, maakn, makna
+ * @param {string} str - input kata yang ingin di-swap dan dicek palindrom
+ * @returns {boolean} true bila kata yang di-swap adalah palindrom
+ */
+
+ // arcecar
+function palindromeSwapper(str) {
+  for (var i = 0; i < str.length; i++) {
+    debugger
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
+      if (i === j && str[j + 1] !== undefined) {
+        newStr += str[j + 1];
+        newStr += str[i];
+        j++;
+      } else {
+        newStr += str[j];
+      }
+    }
+    if (isPalindrome(newStr)) return true;
+  }
+  return false;
+}
+
+/**
+ * @function isPalindrome
+ * Cek apabila kata merupakan palindromeSwapper
+ * @param {string} str - input kata yang dicek bila palindrom
+ * @returns {boolean} true bila kata adalah palindrom
+ */
+function isPalindrome(str) {
+  if (str.split('').reverse().join('') === str) return true;
+  return false;
+}
+
+console.log(palindromeSwapper('arcecar')); // TRUE
+console.log(palindromeSwapper('racecar')); // TRUE
+console.log(palindromeSwapper('recacar')); // FALSE
